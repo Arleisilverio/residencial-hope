15a1796e426ea6ce482c49eb58fca5108d440923
[dyad] <arleisilverio41@gmail.com>
2025-11-15 00:45:30 -0300

    [dyad] Correção do caminho de importação do dashboard - wrote 1 file(s)

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import AdminDashboardPage from './pages/admin/AdminDashboardPage';
+import AdminDashboardPage from './src/pages/admin/AdminDashboardPage';
 
 function App() {
   return (
