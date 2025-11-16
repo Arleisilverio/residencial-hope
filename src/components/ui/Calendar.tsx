7c0a039507e1478ab45224cf56dd5182b0b9ab6e
[dyad] <arleisilverio41@gmail.com>
2025-11-16 17:56:34 -0300

    [dyad] Corrigindo erros de compilação nos componentes de UI. - wrote 2 file(s)

--- src/components/ui/Calendar.tsx
+++ src/components/ui/Calendar.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react"
 import { DayPicker } from "react-day-picker"
-
 import { cn } from "../../lib/utils"
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
