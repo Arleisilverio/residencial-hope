7bb04b72ec382129969bc1f3c92e112aa3869694
[dyad] <arleisilverio41@gmail.com>
2025-11-16 17:47:14 -0300

    [dyad] Corrigindo o componente de calendário e o formulário de adicionar inquilino. - wrote 2 file(s)

--- src/components/admin/AddTenantForm.tsx
+++ src/components/admin/AddTenantForm.tsx
@@ -195,8 +195,10 @@ const AddTenantForm: React.FC<AddTenantFormProps> = ({ availableApartments, onSu
             </PopoverTrigger>
             <PopoverContent className="w-auto p-0 border bg-popover">
               <Calendar
+                mode="single"
                 selected={moveInDate}
                 onSelect={setMoveInDate}
+                initialFocus
               />
             </PopoverContent>
           </Popover>
